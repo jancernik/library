2f8b3d40d002c9db3cf37bcd29d17d6ecbbc1f46
Jan Cernik <jancernik12@gmail.com>
2022-03-05 03:11:42 -0300

    .

--- script.js
+++ script.js
@@ -1,48 +1,161 @@
-// function Book (title, author, pages, read) {
-//   this.title = title;
-//   this.author = author;
-//   this.pages = pages;
-//   this.read = read;
-//   this.info = function() {
-//     return `${this.title}, ${this.author}, ${this.pages}, ${this.read}`
-//   }
-// }
+const booksGrid = document.querySelector(".books-grid");
+const openModalButton = document.querySelector(".add-book");
+const openModalHover = document.querySelector(".add-book-hover");
+const closeModalButton = document.querySelector(".close-modal");
+const addBookModal = document.querySelector(".modal");
+const overlay = document.getElementById("overlay");
+const lightOverlay = document.getElementById("light-overlay");
+const addBookForm = document.getElementById("add-book-form");
+const deleteCardButton = document.querySelectorAll(".deleteCard");
 
-// const book1 = new Book ("The Hobbit", "J. R. R. Tolkien", 304, "not read yet");
-// console.log(book1.info());
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
 
-const addBook = document.querySelector(".add-book");
-addBook.addEventListener("click", () => {
-  createBookCard("dummy title", "dummy author", "dummy pages", "dummy state")
+class Store {
+  static getBooks() {
+    let books;
+    if (localStorage.getItem("books") === null) {
+      books = [];
+    } else {
+      books = JSON.parse(localStorage.getItem("books"));
+    }
+    return books;
+  }
+
+  static addBook(book) {
+    const books = Store.getBooks();
+    books.push(book);
+    localStorage.setItem("books", JSON.stringify(books));
+  }
+
+  static removeBook() {
+    const books = Store.getBooks();
+    localStorage.setItem("books", JSON.stringify(books));
+  }
+}
+
+function displayBooks() {
+  const books = Store.getBooks();
+  books.forEach((book) => {
+    createBookCard(book);
+  });
+}
+
+booksGrid.addEventListener("click", (e) => {
+  const books = Store.getBooks();
+  const bookTitle = e.target.closest("div").firstChild.innerText;
+  if (e.target.classList.contains("delete-card")) {
+    e.target.parentElement.remove();
+    books.forEach((book, index) => {
+      books.splice(index, 1);
+    });
+  }
+  if (e.target.classList.contains("read-state")) {
+    books.forEach((book, index) => {
+      if (book.title === bookTitle) {
+        if (books[index].read) {
+          books[index].read = false;
+          e.target.classList.add("not-read");
+          e.target.classList.remove("read");
+          e.target.textContent = "Not read yet";
+        } else {
+          books[index].read = true;
+          e.target.classList.add("read");
+          e.target.classList.remove("not-read");
+          e.target.textContent = "Already read";
+        }
+      }
+    });
+  }
+  localStorage.setItem("books", JSON.stringify(books));
 });
 
-const booksGrid = document.querySelector(".books-grid");
+document.addEventListener("DOMContentLoaded", displayBooks());
 
-function createBookCard(titleData, authorData, pagesData, readStateData) {
+addBookForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const title = document.querySelector("#title-input").value;
+  const author = document.querySelector("#author-input").value;
+  const pages = document.querySelector("#pages-input").value;
+  const read = document.querySelector("#read-status").checked;
+  const book = new Book(title, author, pages, read);
+  Store.addBook(book);
+  createBookCard(book);
+});
+
+function clearForm() {
+  document.querySelector("#title-input").value = "";
+  document.querySelector("#author-input").value = "";
+  document.querySelector("#pages-input").value = "";
+  document.querySelector("#read-status").checked = false;
+}
+
+function createBookCard(book) {
   const bookCard = document.createElement("div");
-  bookCard.setAttribute("class", "book-card");
-  booksGrid.append(bookCard);
-
+  bookCard.classList.add("book-card");
+  bookCard.classList.add("fade-in");
   const title = document.createElement("h3");
-  title.textContent = titleData;
-  bookCard.append(title);
-
+  title.textContent = book.title;
   const author = document.createElement("p");
-  author.setAttribute("class", "author");
-  author.textContent = authorData;
-  bookCard.append(author);
-
+  author.classList.add("author");
+  author.textContent = book.author;
   const pages = document.createElement("p");
-  pages.setAttribute("class", "pages");
-  pages.textContent = `${pagesData} pages`;
-  bookCard.append(pages);
-
+  pages.classList.add("pages");
+  pages.textContent = `${book.pages} pages`;
   const readState = document.createElement("button");
-  readState.setAttribute("class", "read-state");
-  readState.textContent = readStateData;
-  bookCard.append(readState);
-
+  readState.classList.add("read-state");
   const deleteCard = document.createElement("button");
-  deleteCard.setAttribute("class", "delete-card");
+  deleteCard.classList.add("delete-card");
+  if (book.read) {
+    readState.textContent = "Already read";
+    readState.classList.add("read");
+  } else {
+    readState.textContent = "Not read yet";
+    readState.classList.add("not-read");
+  }
+  booksGrid.append(bookCard);
+  bookCard.append(title);
+  bookCard.append(author);
+  bookCard.append(pages);
+  bookCard.append(readState);
   bookCard.append(deleteCard);
+}
+
+[openModalButton, openModalHover].forEach((item) => {
+  item.addEventListener("click", () => {
+    openModal();
+  });
+  item.addEventListener("mouseover", () => {
+    toggleHoverElements("add");
+  });
+  item.addEventListener("mouseout", () => {
+    toggleHoverElements("remove");
+  });
+});
+
+closeModalButton.addEventListener("click", () => {
+  closeModal();
+});
+overlay.addEventListener("click", () => {
+  closeModal();
+});
+
+function openModal() {
+  addBookModal.classList.add("active");
+  overlay.classList.add("active");
+}
+
+function closeModal() {
+  addBookModal.classList.remove("active");
+  overlay.classList.remove("active");
+  setTimeout(clearForm, 300);
+}
+
+function toggleHoverElements(action) {
+  openModalHover.classList[action]("active");
+  lightOverlay.classList[action]("active");
 }
